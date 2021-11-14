2f7e17c26930a97d188b9207c369976b53a4ffc4
ladebrouille4587 <ladebrouille4587@gmail.com>
2021-11-14 15:16:51 +0100

    remove password from script

--- Samples.IdentityServer4/ProCodeGuide.Samples.IdentityServer4/Migrations/Scripts/Update/identity-server-db-initialization.sh
+++ Samples.IdentityServer4/ProCodeGuide.Samples.IdentityServer4/Migrations/Scripts/Update/identity-server-db-initialization.sh
@@ -1,7 +1,7 @@
 #avec notepadd++ convertir les sauts de ligne au format UNIX
 for i in {1..90};
 do
-    /opt/mssql-tools/bin/sqlcmd -l 30 -S localhost -U sa -P P@ssword1! -i /home/IdentityServer/Migrations/Scripts/Update/20211113105032_CreateDatabase.sql
+    /opt/mssql-tools/bin/sqlcmd -l 30 -S localhost -U sa -P $1 -i /home/IdentityServer/Migrations/Scripts/Update/20211113105032_CreateDatabase.sql
     if [ $? -eq 0 ]
     then
         echo "setup.sql completed"
